01b4171ae1cb881310f822f00c2b997641bdf29f
Serhii Khomenko <cahxoce2101@gmail.com>
2019-06-23 19:09:49 +0300

    first load

---
+++ Lesson 1/js/script.js
@@ -0,0 +1,67 @@
+//Practice
+
+var name = 'Serhii';
+var student;
+student = name;
+console.log(student);
+
+
+var a = 11;
+var b = 2;
+var c = 36;
+var d = 3;
+var e;
+e = c % d;
+console.log(a % b);
+console.log(e);
+
+
+var a = "5",
+    b = 2;
+console.log(a + b);
+
+var str = "Hello";
+var a = 5;
+console.log(str + a);
+
+
+
+var a = 6,
+    b = 3;
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+var a = 3,
+    b = 6;
+if (a != b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+var a = 1,
+    b = "";
+if (a === b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+
+if ('B' > 'A') {
+    console.log('B');
+}
+
+if ('a' > 'Z') {
+    console.log('a');
+}
+
+
+var a = false,
+    b = 0;
+console.log(a >= b);
+
+var a = 2 > 1 ? 'ok' : 'not';
+console.log(a);
---
+++ Lesson 2/js/script.js
@@ -0,0 +1,64 @@
+var result = prompt("Input your digit", 0);
+if (result > 0) {
+    console.log('1');
+} else if (result < 0) {
+    console.log('-1');
+} else {
+    console.log('0');
+}
+
+
+// Practice
+var result = prompt('input', 0);
+switch (result) {
+    case '1':
+        console.log('a')
+        break;
+    case '2':
+        console.log('b')
+        break;
+    case '3':
+        console.log('c')
+        break;
+    default:
+        console.log('z')
+}
+
+
+// вивести в консоль за допомогою циклу квадрати чисел від 1 до 9 
+// for (var i = 1; i < 10; i++) {
+//     console.log(i * i);
+// }
+
+
+var num = ' ';
+for (i = 1; i <= 9; i++) {
+    num += i * i + ' ';
+}
+console.log(num);
+
+
+
+// вивести в консоль за допомогою циклу квадрати чисел від 1 до 9 за допомогою while
+// var i = 1;
+// while (i < 10) {
+//     console.log(i * i);
+//     i++;
+// }
+
+var num = '';
+var i = 1;
+while (i < 10) {
+    num += i * i + ' ';
+    i++;
+}
+console.log(num);
+
+
+
+// ● функція приймає два параметри(числа) і повертає більший з них 
+function calc(number1, number2) {
+    return (number1 > number2) ? number1 : number2;
+}
+var result = calc(20, 6);
+console.log(result);
