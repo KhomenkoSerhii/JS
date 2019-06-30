8e0f4207495383a44e14dd81b486e3f52ad2d236
Serhii Khomenko <cahxoce2101@gmail.com>
2019-06-30 12:25:30 +0300

    some changes

--- HW3/js/script2.js
+++ HW3/js/script2.js
@@ -1,4 +1,4 @@
-// // task 1
+ // task 1
 var min = 10,
     max = 20;
 var r = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -18,23 +18,42 @@ console.log(getNumber());
 
 var arr = ['lorem', 'ipsum', 'is', 'simply', 'dummy'];
 var arr1 = arr.join(' ');
-function titleCase(str) {
 
+function titleCase(str) {
     var splitStr = str.toLowerCase().split(' ');
     for (var i = 0; i < splitStr.length; i++) {
         splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
     return splitStr.join(' ');
 }
+console.log(titleCase(arr1));  //each word with a capital letter
 
-console.log(titleCase(arr1));
 
 
-// // task 3
 
+var arr1 = ['Lorem', 'ipsum', 'is', 'simply', 'dummy'];
+for (var arr of arr1) {
+    // console.log(arr);
+}
+
+var tmp = '';
+for (var arr of arr1) {
+    if (arr[0] == arr[0].toUpperCase()) {
+        tmp += arr;
+    }
+}
+console.log(tmp);
+
+
+
+
+
+
+ // task 3
+var result = String('first string', 'Second one');
 function String(str1, str2) {
 
     return (str1.length > str2.length) ? str1 : str2;
 }
-var result = String('first string', 'Second one');
+
 console.log(result);
